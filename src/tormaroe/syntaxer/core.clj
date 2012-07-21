d7b799aec88131e9b2c12b33216c0e5c7d5bd0bf
tormaroe <torbjorn.maro@gmail.com>
2012-07-22 00:44:05 +0200

    Refactoring and added comments

--- src/tormaroe/syntaxer/core.clj
+++ src/tormaroe/syntaxer/core.clj
@@ -53,24 +53,30 @@
         (recur tokenizers)))))
 
 (defn tokenize
-  ""
-  ([lang-key input] 
-    (tokenize (lang-key tokenizers) 
-              input
-              []))
-  ([lang-tokenizers input tokens]
-    (if-let [t (get-first-token lang-tokenizers input)]
-      (recur lang-tokenizers 
-             (subs input (count (first t)))
-             (conj tokens t))
-      tokens)))
+  "Given sourcecode as input and keyword lang specifying the
+  language used, tokenize will produce a sequence of tokens."
+  [lang input]
+  (letfn [(input-eater [tokenizers input tokens]
+            (if-let [t (get-first-token tokenizers input)]
+              (recur tokenizers
+                     (subs input (count (first t)))
+                     (conj tokens t))
+              tokens))]
+    (input-eater (lang tokenizers) input [])))
 
 ;; HTML ENCODE
 
-(defn char->html--force [c]
+(defn char->html--force 
+  "HTML encodes any character c to format &#n;
+  where n is the numeric character code of c."
+  [c]
   (str "&#" (int c) ";"))
 
-(defn char->html [c]
+(defn char->html 
+  "HTML encodes <<dangerous>> character. If
+  character is not considered dangerous it is
+  returned as is."
+  [c]
   (condp = c
     \< "&lt;"
     \> "&gt;"
@@ -79,10 +85,17 @@
     \' (char->html--force \')
     c))
 
-(defn html-encode [s]
+(defn html-encode 
+  "HTML encodes an entire string according to
+  the rules of char->html"
+  [s]
   (apply str (map char->html s)))
 
-(defn token->html [lang [text type]]
+(defn token->html 
+  "Given a language keyword and a token returns
+  a hiccup vector representing it. Styles are retrieved
+  from the configuration for the language."
+  [lang [text type]]
   (if-let [color (type (lang colors))]
     (if (fn? color) 
       (if-let [dynamic-style (color text)]
@@ -91,16 +104,22 @@
       [:span {:style color} text])
     text))
 
-(defn swap-at [idx f lst]
+(defn swap-at 
+  "Applyes f to the value at index idx of sequence lst,
+  returning the list with the value swapped."
+  [idx f lst]
   (assoc lst idx (f (nth lst idx))))
 
-(defn source->html [lang s]
-  (html
-    [:pre 
-      \newline
-      (->> 
-        (tokenize lang s)
-        (map (partial swap-at 0 html-encode))
-        (map (partial token->html lang))) 
-      \newline]))
+(defn source->html 
+  "Given a language keyword and sourcecode string s,
+  returns HTML formatted according to the configuration
+  for the language."
+  [lang s]
+  (html [:pre 
+         \newline
+         (->> 
+           (tokenize lang s)
+           (map (partial swap-at 0 html-encode))
+           (map (partial token->html lang))) 
+         \newline]))
 
